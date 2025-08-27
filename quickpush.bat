90ed157958a0b01453ed7afd2dd4223cf7fb2a5a
antlaw0 <antlaw0@gmail.com>
2025-08-27 09:42:07 -0500

    Quick Push

--- quickpush.bat
+++ quickpush.bat
@@ -1,5 +1,5 @@
 @echo off
-cd C:\Users\alawl\Desktop\Game Dev Projects\professionalsitev2\professional-website
+cd C:\Users\alawl\Desktop\Game Dev Projects\professionalsitev2\ProfessionalWebsite\newproj\professional-website
 git add .
 git commit -m "Quick Push"
 git push origin main
