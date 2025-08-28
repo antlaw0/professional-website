dc7cf1b04a047eb8326ec5b3967ebf1faf6afbcc
antlaw0 <antlaw0@gmail.com>
2025-08-28 10:34:03 -0500

    Quick Push

---
+++ _site/run.bat
@@ -0,0 +1,4 @@
+@echo off
+cd C:\Users\alawl\Desktop\Game Dev Projects\Professional-website-repo\ProfessionalWebsite\professional-website
+echo starting local Jekyll server on localhost:4000
+bundle exec jekyll serve
